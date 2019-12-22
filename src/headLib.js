2339ad7a16d15cefcec0795cbde4450ec1837f64
Apurva <apurva.gurme@thoughtworks.com>
2019-12-22 13:30:25 +0530

    Added separateAllLines function

--- src/headLib.js
+++ src/headLib.js
@@ -5,9 +5,17 @@ const parseInput = function(userOptions) {
 };
 
 const getFileContents = function(filePath) {
-  const contents = fs.readFileSync(filePath);
-  return contents;
+  if (filePath.exists) {
+    const contents = fs.readFileSync(filePath);
+    return contents;
+  }
+  return [`head: ${filePath}: No such file or directory`];
+};
+
+const separateAllLines = function(contents) {
+  return contents.split('\n');
 };
 
 exports.parseInput = parseInput;
 exports.getFileContents = getFileContents;
+exports.separateAllLines = separateAllLines;
--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -1,4 +1,8 @@
-const { parseInput, getFileContents } = require('../src/headLib');
+const {
+  parseInput,
+  getFileContents,
+  separateAllLines
+} = require('../src/headLib');
 const { assert } = require('chai');
 
 describe('parseInput', function() {
@@ -12,9 +16,28 @@ describe('getFileContents', function() {
   it('should return the contents of file', function() {
     const getFileContents = function(path, encoding) {
       if (path == 'somePath' && encoding == 'utf8') return true;
+      return false;
     };
     const path = 'somePath';
     const encoding = 'utf8';
     assert.isOk(getFileContents(path, encoding));
+  });
+
+  it('should return false is file is not present', function() {
+    const getFileContents = function(path, encoding) {
+      if (path == 'somePat' && encoding == 'utf8') return true;
+      return false;
+    };
+    const path = 'somePath';
+    const encoding = 'utf8';
+    assert.notOk(getFileContents(path, encoding));
+  });
+});
+
+describe('separateAllLines', function() {
+  it('should give all lines separated into an array', function() {
+    const contents = 'one\ntwo\nthree';
+    const expected = ['one', 'two', 'three'];
+    assert.deepStrictEqual(separateAllLines(contents), expected);
   });
 });
