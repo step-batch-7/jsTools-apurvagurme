544433aa1c80d76948c5dd9d07a5125c0c5604f7
Apurva <apurva.gurme@thoughtworks.com>
2019-12-22 12:53:28 +0530

    Added getFileContents function

--- src/headLib.js
+++ src/headLib.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
 const parseInput = function(userOptions) {
   return { filenames: userOptions };
 };
 
+const getFileContents = function(filePath) {
+  const contents = fs.readFileSync(filePath);
+  return contents;
+};
+
 exports.parseInput = parseInput;
+exports.getFileContents = getFileContents;
--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -1,9 +1,20 @@
-const parseInput = require('../src/headLib').parseInput;
+const { parseInput, getFileContents } = require('../src/headLib');
 const { assert } = require('chai');
 
 describe('parseInput', function() {
   it('should give the userInputs', function() {
     const expected = { filenames: ['file1'] };
     assert.deepStrictEqual(parseInput(['file1']), expected);
+  });
+});
+
+describe('getFileContents', function() {
+  it('should return the contents of file', function() {
+    const getFileContents = function(path, encoding) {
+      if (path == 'somePath' && encoding == 'utf8') return true;
+    };
+    const path = 'somePath';
+    const encoding = 'utf8';
+    assert.isOk(getFileContents(path, encoding));
   });
 });
