75fd47facc0c80a6e3462c0344d03504d8d9154b
Apurva <apurva.gurme@thoughtworks.com>
2019-12-27 19:25:01 +0530

    Modified main function

--- head.js
+++ head.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
-const process = require('process');
+const { stderr, stdout, stdin } = process;
+const { readFileSync, existsSync } = fs;
 const { performHead } = require('./src/headOperation');
+
+const display = function(output) {};
 
 const main = function() {
   const cmdLineArgs = process.argv.slice(2);
-  const { error, lines } = performHead(cmdLineArgs, fs, process);
+  const { error, lines } = performHead(
+    cmdLineArgs,
+    { readFileSync, existsSync },
+    { stderr, stdout, stdin }
+  );
   process.stderr.write(error);
   process.stdout.write(lines);
 };
